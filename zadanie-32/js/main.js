4c38e98d52d8ac8033eb702e2cf5059ea6413f35
mkuczma <marcin.kuczma@gmail.com>
2022-11-25 13:42:59 +0100

    32-js-wieksza-liczba-Poprawione

--- zadanie-32/js/main.js
+++ zadanie-32/js/main.js
@@ -1,7 +1,7 @@
 let num1 = 2;
 let num2 = 5;
-function bigger (num1, num2) {
-    let result = Math.max(num1, num2);
+function bigger (firstNumber, secondNumber) {
+    let result = Math.max(firstNumber, secondNumber);
     return result;
 };
 console.log( bigger (num1, num2) );
